8fe6d16f9a906a818d84e88a5b5588b997ef617e
Simon Katz <sk@nomistech.com>
2020-07-04 18:55:34 +0100

    Whitespace change to force rebuild

--- src/film_ratings/main.clj
+++ src/film_ratings/main.clj
@@ -6,7 +6,8 @@
 (duct/load-hierarchy)
 
 (defn -main [& args]
-  (let [keys (or (duct/parse-keys args) [:duct/migrator :duct/daemon])]
+  (let [keys (or (duct/parse-keys args)
+                 [:duct/migrator :duct/daemon])]
     (-> (duct/read-config (io/resource "film_ratings/config.edn"))
         (duct/prep keys)
         (duct/exec keys))))
