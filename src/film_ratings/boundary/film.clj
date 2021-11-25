60bafaa4a86ad642ac69c5752408cee6a83de01c
Simon Katz <sk@nomistech.com>
2021-11-25 15:50:59 +0000

    Add search functionality

--- src/film_ratings/boundary/film.clj
+++ src/film_ratings/boundary/film.clj
@@ -5,17 +5,22 @@
 
 (defprotocol FilmDatabase
   (list-films [db])
+  (fetch-films-by-name [db name])
   (create-film [db film]))
 
 (extend-protocol FilmDatabase
   duct.database.sql.Boundary
   (list-films [{db :spec}]
     (jdbc/query db ["SELECT * FROM film"]))
+  (fetch-films-by-name [{db :spec} name]
+    (let [search-term (str "%" name "%")]
+     (jdbc/query db ["SELECT * FROM film WHERE LOWER(name) like LOWER(?)" search-term])))
   (create-film [{db :spec} film]
     (try
-      (let [result (jdbc/insert! db :film film)]
-        (if-let [id (val (ffirst result))]
-          {:id id}
-          {:errors ["Failed to add film."]}))
-      (catch SQLException ex
-        {:errors [(format "Film not added due to %s" (.getMessage ex))]}))))
+     (let [result (jdbc/insert! db :film film)]
+       (if-let [id (val (ffirst result))]
+         {:id id}
+         {:errors ["Failed to add film."]}))
+     (catch SQLException ex
+       ;; (log/errorf "Failed to insert film. %s\n" (.getMessage ex))
+       {:errors [(format "Film not added due to %s" (.getMessage ex))]}))))
--- src/film_ratings/handler/index.clj
+++ src/film_ratings/handler/index.clj
@@ -1,5 +1,6 @@
 (ns film-ratings.handler.index
-  (:require [ataraxy.core :as ataraxy]
+  (:require [ataraxy.core ; :as ataraxy
+             ]
             [ataraxy.response :as response]
             [film-ratings.views.index :as views.index]
             [integrant.core :as ig]))
